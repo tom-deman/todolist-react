21eedd8b5b23b8133a6ca05daf24c529c967c447
Thomas <dt.deman.thomas@gmail.com>
2020-03-15 20:52:14 +0100

    Updates

---
+++ precache-manifest.38747e144b790c602cb21efba69e53ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "996025f8becf81c8cfa0d11431b32326",
+    "url": "/todolist-react/index.html"
+  },
+  {
+    "revision": "b5ad5ebd7b11fdee50ae",
+    "url": "/todolist-react/static/css/main.58c2da8e.chunk.css"
+  },
+  {
+    "revision": "3a8fe9f0e169686ecc7e",
+    "url": "/todolist-react/static/js/2.4a8f2b6c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/todolist-react/static/js/2.4a8f2b6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5ad5ebd7b11fdee50ae",
+    "url": "/todolist-react/static/js/main.227ce4f1.chunk.js"
+  },
+  {
+    "revision": "6e86028f6677b0cb4b1f",
+    "url": "/todolist-react/static/js/runtime-main.e492f81a.js"
+  }
+]);
